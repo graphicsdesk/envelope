4501c95f84d604f0a016c068c6feecf45a1a88a4
apark2020 <ajp2259@columbia.edu>
2023-02-17 16:36:27 -0500

    pub ready

---
+++ src/scripts/scrollscript.js
@@ -0,0 +1,19 @@
+d3.selectAll("#step-five").classed('spacer_margin', true) 
+const scroller = scrollama();
+
+scroller
+    .setup({
+        step: "#scrolly .scrolly-overlay .step",
+        offset: 0.5,
+        debug: false
+    })
+    .onStepEnter(function({ element, index, direction}) {
+        const event = new CustomEvent('stepin', { detail: { direction: direction } })
+        element.dispatchEvent(event);
+    })
+    .onStepExit(function({ element, index, direction }) {
+        const event = new CustomEvent('stepout', { detail: { direction: direction } })
+        element.dispatchEvent(event);
+    });
+
+window.addEventListener("resize", scroller.resize);
